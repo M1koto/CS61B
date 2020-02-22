683072a95cd006b949fd2bf798cfc1d8e4f98709
Kenny Liao <yenming891109@berkeley.edu>
2020-02-21 23:13:14 -0800

    started with alphabet without testing

--- proj1/enigma/Alphabet.java
+++ proj1/enigma/Alphabet.java
@@ -2,7 +2,7 @@ package enigma;
 
 /** An alphabet of encodable characters.  Provides a mapping from characters
  *  to and from indices into the alphabet.
- *  @author
+ *  @author kenny Liao
  */
 class Alphabet {
 
@@ -10,6 +10,7 @@ class Alphabet {
      *  K (numbering from 0). No character may be duplicated. */
     Alphabet(String chars) {
         // FIXME
+        _chars = chars;
     }
 
     /** A default alphabet of all upper-case characters. */
@@ -19,24 +20,28 @@ class Alphabet {
 
     /** Returns the size of the alphabet. */
     int size() {
-        return 26; // FIXME
+        return _chars.length(); // FIXME
     }
 
     /** Returns true if CH is in this alphabet. */
     boolean contains(char ch) {
-        return 'A' <= ch && ch <= 'Z'; // FIXME
+        return _chars.indexOf(ch) > 0; // FIXME
     }
 
     /** Returns character number INDEX in the alphabet, where
      *  0 <= INDEX < size(). */
     char toChar(int index) {
-        return (char) ('A' + index); // FIXME
+        return _chars.charAt(index); // FIXME
     }
 
     /** Returns the index of character CH which must be in
      *  the alphabet. This is the inverse of toChar(). */
     int toInt(char ch) {
-        return ch - 'A'; // FIXME
+        return _chars.indexOf(ch);
+        // FIXME
     }
+
+    /** class variable - self work*/
+    public String _chars;
 
 }
