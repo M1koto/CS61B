7f1ed4615b9c8d0e2d6de1b663d669ffd48f8967
Kenny Liao <yenming891109@berkeley.edu>
2020-04-09 21:03:06 +0800

    finished this time

--- hw7/Sum.java
+++ hw7/Sum.java
@@ -1,11 +1,24 @@
 /** HW #7, Two-sum problem.
- * @author
+ * @author KENNY L
  */
 public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
-        return false;  // REPLACE WITH YOUR ANSWER
+        MySortingAlgorithms.MergeSort t = new MySortingAlgorithms.MergeSort();
+        t.sort(A, A.length);
+        t.sort(B, B.length);
+        for (int i = 0; i < A.length; i++) {
+            int keep = m - A[i];
+            for (int j = 0; j < B.length; j++) {
+                if (keep - B[j] == 0) {
+                    return true;
+                } else if (keep - B[j] < 0) {
+                    break;
+                }
+            }
+        }
+        return false;
     }
 
 }
