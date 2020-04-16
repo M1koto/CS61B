3df43c7ed6dd33a21d5e2ccaddee7d593cdb23dc
Kenny Liao <yenming891109@berkeley.edu>
2020-04-16 17:26:17 +0800

    started on main switches and set up files

--- proj3/gitlet/DoubleHT.java
+++ proj3/gitlet/DoubleHT.java
@@ -1,4 +1,49 @@
 package gitlet;
 
-public class DoubleHT {
+import java.util.LinkedList;
+
+public class DoubleHT{
+    DoubleHT(DoubleHT Parent, Commit C) {
+        _parent1 = Parent;
+        _data = C;
+    }
+
+
+
+
+
+
+
+
+
+
+    /** The commit stored in this DoubleHT. */
+    private Commit _data;
+
+    /** The first branch of where this DoubleHT is on.
+     * This field is never null. */
+    private String _branch1;
+
+    /** The second branch of where this DoubleHT is on.*/
+    private String _branch2;
+
+    /** The first parent of this DoubleHT.
+     * Is never null except for the first ever commit. */
+    private DoubleHT _parent1;
+
+    /** The second parent of this DoubleHT.
+     * Null if does not exist.
+     */
+    private DoubleHT _parent2;
+
+    /** The first child of this DoubleHT.
+     * If doesn't have any, then is null.
+     */
+    private DoubleHT _child1;
+
+    /** The second child of this DoubleHT.
+     * If doesn't have any, then is null.
+     */
+    private DoubleHT _child2;
 }
+
--- proj3/gitlet/Main.java
+++ proj3/gitlet/Main.java
@@ -1,14 +1,65 @@
 package gitlet;
 
-/** Driver class for Gitlet, the tiny stupid version-control system.
- *  @author
+/**
+ * Driver class for Gitlet, the tiny stupid version-control system.
+ *
+ * @author
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND> .... */
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND> ....
+     */
     public static void main(String... args) {
-        // FILL THIS IN
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
+        switch (args[0]) {
+            case "init":
+                new User();
+                break;
+            case "add":
+                //FIXME
+                break;
+            case "commit":
+                //FIXME
+                break;
+            case "rm":
+                //FIXME
+                break;
+            case "log":
+                //FIXME
+                break;
+            case "global-log":
+                //FIXME
+                break;
+            case "find":
+                //FIXME
+                break;
+            case "status":
+                //FIXME
+                break;
+            case "checkout":
+                //FIXME
+                break;
+            case "branch":
+                //FIXME
+                break;
+            case "rm-branch":
+                //FIXME
+                break;
+            case "reset":
+                //FIXME
+                break;
+            case "merge":
+                //FIXME
+                break;
+            default:
+                System.out.println("No command with that name exists.");
+                System.exit(0);
+        }
     }
 
 }
