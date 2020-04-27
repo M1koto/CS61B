e923132efb0e2c6594e6ea254cedd2e5ec46db1f
Kenny Liao <yenming891109@berkeley.edu>
2020-04-27 17:31:48 +0800

    tested all pass and fixed spacing for switch

--- proj3/gitlet/Main.java
+++ proj3/gitlet/Main.java
@@ -34,94 +34,94 @@ public class Main {
             System.exit(0);
         }
         switch (args[0]) {
-            case "init":
-                if (_user != null) {
-                    System.out.println("A Gitlet version-control system already exists in the current directory.");
-                    break;
-                }
-                _user = new User();
-                _user.save();
+        case "init":
+            if (_user != null) {
+                System.out.println("A Gitlet version-control system already exists in the current directory.");
                 break;
-            case "add":
-                File temp = new File(args[1]);
-                if (!temp.exists()) {
-                    System.out.println("File does not exist.");
-                    System.exit(0);
-                }
-                _user.add(temp);
-                _user.save();
-                break;
-            case "commit":
-                _user.commit(split(args));
-                _user.save();
-                break;
-            case "rm":
-                File temp2 = new File(args[1]);
-                _user.rm(temp2);
-                _user.save();
-                break;
-            case "log":
-                _user.log();
-                break;
-            case "global-log":
-                _user.global();
-                break;
-            case "find":
-                _user.find(split(args));
-                break;
-            case "status":
-                _user.status();
-                break;
-            case "checkout":
-                File temp3;
-                if (_user.warning()) {
-                    System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
-                    System.exit(0);
-                }
-                if (args[1].equals("--")) {
-                    temp3 = new File(args[2]);
-                    _user.checkout(_user.getH().getCommit().getCode(), temp3);
-                } else {
-                    if (args.length > 2 && args[2].equals("--")) {
-                        temp3 = new File(args[3]);
-                        _user.checkout(args[1], temp3);
-                    } else if (args.length == 2){
-                        _user.switchBranch(args[1]);
-                        _user.checkAll();
-                    }
-                    else {
-                        System.out.println("Incorrect operands.");
-                        System.exit(0);
-                    }
-                }
-                _user.save();
-                break;
-            case "branch":
-                _user.addBranch(args[1]);
-                _user.save();
-                break;
-            case "rm-branch":
-                _user.rmBranch(args[1]);
-                _user.save();
-                break;
-            case "reset":
-                if (_user.warning()) {
-                    System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
-                    System.exit(0);
-                }
-                _user.reset(args[1]);
-                _user.save();
-                break;
-            case "merge":
-                if (_user.warning()) {
-                    System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
-                    System.exit(0);
-                }
-                _user.merge(args[1]);
-                break;
-            default:
-                System.out.println("No command with that name exists.");
+            }
+            _user = new User();
+            _user.save();
+            break;
+        case "add":
+            File temp = new File(args[1]);
+            if (!temp.exists()) {
+                System.out.println("File does not exist.");
                 System.exit(0);
+            }
+            _user.add(temp);
+            _user.save();
+            break;
+        case "commit":
+            _user.commit(split(args));
+            _user.save();
+            break;
+        case "rm":
+            File temp2 = new File(args[1]);
+            _user.rm(temp2);
+            _user.save();
+            break;
+        case "log":
+            _user.log();
+            break;
+        case "global-log":
+            _user.global();
+            break;
+        case "find":
+            _user.find(split(args));
+            break;
+        case "status":
+            _user.status();
+            break;
+        case "checkout":
+            File temp3;
+            if (_user.warning()) {
+                System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
+                System.exit(0);
+            }
+            if (args[1].equals("--")) {
+                temp3 = new File(args[2]);
+                _user.checkout(_user.getH().getCommit().getCode(), temp3);
+            } else {
+                if (args.length > 2 && args[2].equals("--")) {
+                    temp3 = new File(args[3]);
+                    _user.checkout(args[1], temp3);
+                } else if (args.length == 2){
+                    _user.switchBranch(args[1]);
+                    _user.checkAll();
+                }
+                else {
+                    System.out.println("Incorrect operands.");
+                    System.exit(0);
+                }
+            }
+            _user.save();
+            break;
+        case "branch":
+            _user.addBranch(args[1]);
+            _user.save();
+            break;
+        case "rm-branch":
+            _user.rmBranch(args[1]);
+            _user.save();
+            break;
+        case "reset":
+            if (_user.warning()) {
+                System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
+                System.exit(0);
+            }
+            _user.reset(args[1]);
+            _user.save();
+            break;
+        case "merge":
+            if (_user.warning()) {
+                System.out.println("There is an untracked file in the way; delete it, or add and commit it first.");
+                System.exit(0);
+            }
+            _user.merge(args[1]);
+            break;
+        default:
+            System.out.println("No command with that name exists.");
+            System.exit(0);
         }
     }
 
